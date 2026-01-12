8dab21ddf0ee1e5ab239d647f99a518a355611ee
Lucila Werner <lucilaw@tamarac.org>
2026-01-12 11:14:49 -0500

    css image width

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,20 +13,18 @@ function Header() {
         zIndex: 1000
       }}
     >
-<img
-  src="/images/Patterson.png"
-  alt="Gobble Gobble"
-  style={{
-    display: "block",
-    margin: "0 auto 10px auto",
-    width: "100%",
-    maxWidth: "600px",
-    height: "auto",
-    objectFit: "contain"
-  }}
-/>
-
-
+      <img
+        src="/images/Patterson.png"
+        alt="Gobble Gobble"
+        style={{
+          display: "block",
+          margin: "0 auto 10px auto",
+          width: "100%",
+          maxWidth: "200px",
+          height: "auto",
+          objectFit: "contain"
+        }}
+      />
 
       <h1
         style={{
@@ -36,11 +34,12 @@ function Header() {
           lineHeight: "1.2"
         }}
       >
-       K.I.N.D<br /> RIDE FOR MILES
+        K.I.N.D<br /> RIDE FOR MILES
       </h1>
     </header>
   );
 }
 
 export default Header;
+
 
