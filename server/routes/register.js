de72471e06132f68e3458e5eebbae7e4ee6c7241
Lucila Werner <lucilaw@tamarac.org>
2026-01-29 15:34:15 -0500

    Waiver logic and timestamp

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const db = require("../db");
 
 router.post("/register", (req, res) => {
-  const { firstName, lastName, dob, address, email, phone } = req.body;
+  const { firstName, lastName, dob, address, email, phone, waiverAccepted, waiverTimestamp } = req.body;
 
-
+if (!waiverAccepted || !waiverTimestamp) {
+     return res.status(400).json({ 
+        success: false, 
+        message: "You must accept the waiver before registering." 
+    }); 
+}
   const checkSql = "SELECT * FROM users WHERE email = ?";
 
   db.query(checkSql, [email], (err, results) => {
@@ -21,13 +26,13 @@ router.post("/register", (req, res) => {
 
 
     const insertSql = `
-      INSERT INTO users (first_name, last_name, dob, address, email, phone)
-      VALUES (?, ?, ?, ?, ?, ?)
+      INSERT INTO users (first_name, last_name, dob, address, email, phone, waiver_accepted, waiver_timestamp, registration_timestamp)
+      VALUES (?, ?, ?, ?, ?, ?,?,?,?)
     `;
 
     db.query(
       insertSql,
-      [firstName, lastName, dob, address, email, phone],
+      [firstName, lastName, dob, address, email, phone, waiverAccepted, waiverTimestamp, registrationTimeStamp],
       (err) => {
         if (err) return res.status(500).send(err);
 
