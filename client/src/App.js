4817f9a4f8892cc307f70e5faf80e824f11ae952
Lucila Werner <lucilaw@tamarac.org>
2026-02-25 09:52:00 -0500

    download page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppMain from "./AppMain";
 import ProtectedPage from "./ProtectedPage";
-
-
+import LoginPage from "./LoginPage"; // ← tu formulario de login
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Main page */}
+        {/* Main Page */}
         <Route path="/" element={<AppMain />} />
 
-        {/* Protected page*/}
-        <Route path="/login" element={<ProtectedPage />} />
+        {/* Login Page*/}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Protected Page*/}
+        <Route path="/dashboard" element={<ProtectedPage />} />
+
+             {/* Download Page */}
+        <Route path="/download" element={<ProtectedPage><DownloadPage /></ProtectedPage>} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+
