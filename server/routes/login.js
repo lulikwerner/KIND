b650bb218ec399aa53f5b1cbef513dc3465e1f72
Lucila Werner <lucilaw@tamarac.org>
2026-02-25 09:09:37 -0500

    logs

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,6 +6,10 @@ const CORRECT_PASSWORD = process.env.PROTECTED_PW;
 router.post("/login", (req, res) => {
   const { password } = req.body;
 
+
+  console.log("PW del .env:", process.env.PROTECTED_PW);
+  console.log("PW recibida del frontend:", password);
+
   if (password === CORRECT_PASSWORD) {
     return res.json({ ok: true });
   }
